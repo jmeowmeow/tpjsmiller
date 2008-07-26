be66f0f7ad3012e12fc93589eede43826a545a58
Jeff Miller <jeffrey.miller@gmail.com>
2008-07-26 16:06:11 -0700

    Initial version of Thousand Parsec name generator and demonstration Google App Engine application by Jeff Miller for consideration by Thousand Parsec team.

---
+++ appengine/tpnames/tpnames.py
@@ -0,0 +1,70 @@
+import os
+import time
+import wsgiref.handlers
+
+import names
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.RequestHandler):
+    """Processes name generator requests and loads the web template."""
+    def p(self, str):
+      self.response.out.write(str)
+
+    def getFromRequest(self):
+        randomseed = self.request.get("randomseed", default_value=None)
+
+        if (randomseed):
+            seed = hash(time.time())
+        else:
+            seed = self.request.get("seed", default_value=hash(time.time()))
+            try:
+                intseed = int(seed)
+                seed = intseed
+            except ValueError: 
+                seed = self.request.get("seed", default_value=hash(time.time()))
+                pass
+
+        selected = self.request.get("source", default_value="starwars.txt")
+        return (seed, selected)
+
+    def get(self):
+        (seed, selected) = self.getFromRequest()
+        try:
+            generatedNames = names.genFromFile(selected, seed)
+        except:
+            self.response.out.write("<p><em>File not found:</em> "+ selected)
+            self.response.out.write("</p><p><a href=\"" + self.request.path + "\">Reload form</a></p>")
+            return
+        sources = [ 'persian.txt', 'greek.txt', 'middle-earth.txt', 
+                    'starwars.txt', 'ellie-anne-kate.txt', 'gems.txt',
+                    'western.txt' ]
+        unselected_sources = [ x for x in sources if x != selected ]
+        template_values = {
+          'seed'     : seed,
+          'names'    : generatedNames,
+          'sources'  : unselected_sources, 
+          'selected' : selected,
+          'randomseed' : self.request.get("randomseed", default_value=None,),
+          'sourcefile' : self.readContentsOf(selected),
+          }
+        path = os.path.join(os.path.dirname(__file__), 'index.django')
+        self.p(template.render(path, template_values))
+
+    def readContentsOf(self, selected):
+        try:
+            path = os.path.join(os.path.dirname(__file__), selected)
+            f = open(path, 'r')
+            return f.read()
+        except IOError:
+            return ""
+
+def main():
+    application = webapp.WSGIApplication(
+                                       [('/', MainPage)],
+                                       debug=True)
+    wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == "__main__":
+  main()
---
+++ source/tpnames/names.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+import random
+import re
+import sys
+import time
+    
+def main():
+   names = generate(sys.stdin, None)
+   for name in names:
+     print name
+
+def genFromFile(filename, seed):
+   file = open(filename, 'r')
+   return generate(file, seed)
+
+def generate(input, seed):
+    if (seed == None):
+      seed = time.time() 
+
+    if not isinstance(seed, (int, long, float, complex)):
+      seed = hash(seed)
+
+    syllabary = makeSyllabary(input)
+    names = list()
+    for i in range(15):
+      generatedWord = generateWord(seed + i, syllabary)
+      names.append(generatedWord)
+    return names
+
+def makeSyllabary(input):
+    """Breaks each line down into syllables, returning a list of lists of syllables.
+       Upper case syllables after the first are prepended with a space."""
+    syllabary = list()
+    startsWithUpper = re.compile('^[A-Z]')
+    for line in input:
+        line = line.strip()
+        if not line.startswith('#'):
+            raw_syllables = line.strip().split(' ')
+            syllables = list()
+            for i in range(len(raw_syllables)):
+                syllable = raw_syllables[i]
+                if i > 0 and startsWithUpper.search(syllable):
+                    syllables.append(' ' + syllable)
+                else:
+                    syllables.append(syllable)
+            if len(syllables) >= 2:
+                syllabary.append(syllables)
+    return syllabary
+    
+def generateWord(seed, syllabary):
+    r = random.Random(seed)
+    choice = r.randrange(0, len(syllabary))
+    sourceword = syllabary[choice]
+    generatedWord = sourceword[0]
+    syllableIndex = 0
+    while True:
+        syllableIndex += 1
+        choice = r.randrange(0, len(syllabary))      
+        randomSourceWord = syllabary[choice]
+        lastSyllableIndex = len(randomSourceWord) - 1
+        if lastSyllableIndex <= syllableIndex:
+            syllable = randomSourceWord[-1]
+            generatedWord += syllable
+            break
+        else:
+            syllable = randomSourceWord[syllableIndex]
+            generatedWord += syllable
+    return generatedWord
+    
+if __name__ == "__main__":
+    main()
